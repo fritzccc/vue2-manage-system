240e8ce6d7f73144518f2e849c7920a946441a5c
Fritz <fredcfj@gmail.com>
2018-06-26 23:03:26 +0900

    添加vue-test-utils官方推荐设置 https://vue-test-utils.vuejs.org/zh/guides/testing-SFCs-with-mocha-webpack.html

--- test/unit/webpack.config.js
+++ test/unit/webpack.config.js
@@ -1,4 +1,12 @@
+const nodeExternals = require('webpack-node-externals')
 module.exports = {
+  externals: [nodeExternals()],
+  devtool: 'inline-cheap-module-source-map',
+  output: {
+    // IDE Optimization
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
   module: {
     rules: [
       {
